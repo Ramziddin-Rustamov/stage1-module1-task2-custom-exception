23a648f094fadbe937f21cdb63dc81a71068a1bf
Ramziddin7 <rustamovramziddin7@gmail.com>
2024-03-03 00:11:15 +0100

    all edded

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -3,19 +3,26 @@ package com.epam.mjc;
 
 public class StudentManager {
 
-  private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    private static final long[] IDs={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
-  }
-
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
-
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public Student find(long studentID) {
+        return Student.getValueOf(studentID);
     }
 
-  }
+    public static void main(String[] args) {
+        StudentManager manager=new StudentManager();
+
+        for (int i=0; i < IDs.length; i++) {
+            try {
+                Student student=manager.find(IDs[i]);
+                if (student != null) {
+                    System.out.println("Student name: " + student.getName());
+                } else {
+                    throw new CustomException("Student not found in our database");
+                }
+            } catch (CustomException e) {
+                System.err.println("Exception caught: " + e.getMessage());
+            }
+        }
+    }
 }
